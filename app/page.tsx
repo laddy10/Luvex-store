4e8a1535b884944d45fa097e717dbd5f3ca0aac2
laddy10 <laddy.sofi@gmail.com>
2026-09-19 14:35:17 -0500

    Use better framed AirPods Pro image for playlist editorial

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
       >
         <Link href={`/producto/${pro.slug}`} className="editorial-audio">
           <Image
-            src={`${pro.folder}/AirPods-13.png`}
+            src={`${pro.folder}/AirPods-7.png`}
             alt="AirPods Pro 3 blancos sobre una superficie de mármol"
             fill
             sizes="(max-width: 700px) 100vw, 50vw"
